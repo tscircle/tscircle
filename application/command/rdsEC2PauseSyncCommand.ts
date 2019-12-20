3258a894f7f5e4ea51c469f0cb41c1f7ddf55b6f
henriklippke <h.lippke@exporo.com>
2019-12-20 22:23:41 +0100

    feature/pause-bastion-host

---
+++ application/command/rdsEC2PauseSyncCommand.ts
@@ -0,0 +1,9 @@
+import {database} from '@tscircle/framework/database/database';
+
+exports.handler = async (event, context, callback) => {
+    //TODO if rds event is something like pause
+        //then halt EC2 Instance
+
+    //TODO if rds event is something like started
+        //then start EC2 Instance again
+};
